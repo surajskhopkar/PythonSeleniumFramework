aabc926df2c7be2fe8cfab4e34cbc881be517275
surajkhopkar <suraj.khopkar@icloud.com>
2025-01-23 21:41:30 +0530

    Added new test for passing data through test data file

---
+++ TestData/HomePageData.py
@@ -0,0 +1,5 @@
+
+
+class HomePageData:
+
+    test_homepagedata = [{"firstname":"John","email":"john@gmail.com","password":"12345"},{"firstname":"David","email":"david@gmail.com","password":"12345"}]
